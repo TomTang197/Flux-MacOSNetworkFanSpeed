e5b5534df0d260067cc62bb1dc84b8526dc89a65
BkMahto <bkumar@elluminatiinc.com>
2026-01-29 13:52:41 +0530

    Initial Commit

---
+++ NetworkSpeedMeter/NetworkSpeedMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSpeedMeterApp.swift
+//  NetworkSpeedMeter
+//
+//  Created by Bandan.K on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct NetworkSpeedMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
